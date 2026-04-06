46ade5cd754ec24083961603487dc40c9008e5a6
Raphael Molterer <r.molterer@students.htl-leonding.ac.at>
2026-04-06 17:21:19 +0200

    Refactor game structure: update script path and enhance layout with interactive elements

---
+++ project/scripts/gameScript.js
@@ -0,0 +1,34 @@
+let slides = [
+    {
+        img: "./images/Bild_1.png",
+        text: "Du bist ein Weltenwaechter..."
+    },
+    {
+        img: "./images/Bild_2.png",
+        text: "Die Dimensionen sind durcheinander geraten..."
+    },
+    {
+        img: "./images/Bild_3.png",
+        text: "Du musst die Dimensionen bereisen und helfen."
+    }
+];
+
+let currentSlide = 0;
+
+function next() {
+    currentSlide++;
+    if (currentSlide >= slides.length){ 
+        currentSlide = 0;
+    }
+    showSlide();
+}
+
+function showSlide() {
+    document.getElementById("image").style.backgroundImage =
+        `url(${slides[currentSlide].img})`;
+
+    document.getElementById("text").innerText =
+        slides[currentSlide].text;
+}
+
+showSlide();
