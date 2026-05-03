4cd740a683356a054cf9044338e0c5e0431d56c7
Raphael Molterer <r.molterer@students.htl-leonding.ac.at>
2026-05-03 21:11:21 +0200

    Refactor story structure: migrate story data to new file and remove old JSON

--- project/scripts/gameScript.js
+++ project/scripts/gameScript.js
@@ -124,7 +124,7 @@ let gameData = {
             }
         ]
     }
-};
+}; 
 
 let currentScene = "start";
 
---
+++ project/scripts/story.js
@@ -0,0 +1,81 @@
+let gameData = {
+    "player": {
+        "karma": 0,
+        "inventory": []
+    },
+    "start": {
+        "text": "Du landest in einem Wald, umgeben von hohen Baeumen und dem Zwitschern der Voegel. Vor dir liegt ein Pfad, der in zwei Richtungen fuehrt.",
+        "background": "./game_images/background_forest.jpg",
+        "character": "./game_images/character_wegweiser.png",
+        "choices": [
+            {
+                "text": "Links (Tiefer in den Wald)",
+                "next": "dark_forest",
+                "gain_item": "donut"
+            },
+            {
+                "text": "Rechts (Raus aus den Wald)",
+                "next": "city"
+            }
+        ]
+    },
+    "dark_forest": {
+        "text": "'Hallo, ich bin Wirt, ich kenn dich gar nicht, aber wenn ich du waere wuerde ich schnell aus diesem Wald raus, denn hier ist ein Monster. Was, du hast noch nicht von dem Monster gehoert? Ist auch egal, renn so lange du noch kannst! Ich muss fuer meinen Bruder hier bleiben. Aber ich kann dir was mit auf dem Weg geben, hier.'*Du erhaelst einen Donut*",
+        "background": "./game_images/background_dark_forest.jpg",
+        "character": "./game_images/character_btgw_Wirt.png",
+        "choices": [
+            {
+                "text": "Trotzdem im Wald weitergehen",
+                "next": "monster1",
+                "karma": -3
+            },
+            {
+                "text": "Schnell den Wald verlassen",
+                "next": "city"
+            }
+        ]
+    },
+    "city": {
+        "text": "Du kommst in eine sehr belebte Stadt mit einigen seltsamen Gestalten. Was moechtest du als erstes nach Boesen erkunden?",
+        "background": "./game_images/background_city.jpg",
+        "choices": [
+            {
+                "text": "Schule",
+                "next": "school"
+            },
+            {
+                "text": "Bibliothek",
+                "next": "library"
+            }
+        ]
+    },
+    "library": {
+        "text": "Du betrittst die Bibliothek und siehst einen hungrigen pinken Hasen, dieser stellt sich sofort vor: 'Hallo, ich bin Richard, meine Tochter Annaise hat mich hier her gebracht damit ich auch ein Buch lese, aber ich bin so hungrig... wenn ich doch nur nach Hause damit ich etwas essen koennte... Hast du etwas fuer mich das ich essen kann?'",
+        "background": "./game_images/background_library.jpg",
+        "character": "./game_images/character_gb_Richard.png",
+        "choices": [
+            {
+                "text": "Leider nicht *du verlaesst wieder die Bibliothek und gehst weiter*",
+                "next": "school",
+                "karma": -2
+            },
+            {
+                "text": "Ja, hier hast du einen Donut *du gibst Richard den Donut*",
+                "next": "richard_thanks",
+                "karma": 2,
+                "required_item": "donut"
+            }
+        ]
+    },
+    "richard_thanks": {
+        "text": "Richard dankt dir herzlich fuer den Donut und sagt: 'Das war sehr nett von dir! Ich fuehle mich jetzt viel besser... Ich sollte jetzt schlafen gehen, viel Spass dir noch!'",
+        "background": "./game_images/background_library.jpg",
+        "character": "./game_images/character_gb_Richard.png",
+        "choices": [
+            {
+                "text": "Weiter in die Schule gehen",
+                "next": "school"
+            }
+        ]
+    }
+};
